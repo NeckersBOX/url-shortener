744b0abb00cbd5e89ca1811226581de11acab7a8
Davide Francesco Merico <hds619@gmail.com>
2016-12-02 23:39:17 +0100

    Add MongoDB integration

--- server.js
+++ server.js
@@ -1,14 +1,73 @@
-'use strict';
+"use strict";
 
+const db_data = require ('./db_data').db;
+const mongodb = require ('mongodb').MongoClient;
 const express = require ('express');
 const fs = require ('fs');
 const marked = require('marked');
+const co = require('co');
 
 let app = express ();
 
+app.get ('/add/:link', (req, res) => {
+  res.writeHead (200, { 'Content-Type': 'application/json' });
+
+  /* TODO: validate link */
+  co (function* () {
+    let db = yield mongodb.connect (db_data);
+    if ( !db ) {
+      res.end (JSON.stringify ({ error: 'Error in mongodb.connect' }));
+      return;
+    }
+
+    let collection = db.collection ('links');
+    let base_url = 'https://shorting.herokuapp.com/';
+
+    /* Check if it already exists */
+    let result = yield collection.findOne ({
+      original_url: req.params.link
+    });
+
+    if ( result ) {
+      res.end (JSON.stringify ({
+        original_url: result.original_url,
+        short_url: base_url + result.short_url
+      }));
+
+      db.close ();
+      return;
+    }
+
+    /* Find last id */
+    let last_record = yield collection.findOne ({}, { 'sort': { _id: -1 } });
+
+    let next_id = 1;
+    if ( last_record )
+      next_id = last_record._id + 1;
+
+    /* TODO: Short url id generator */
+    
+    /* Insert new record */
+    collection.insert ({
+      _id: next_id,
+      original_url: req.params.link,
+      short_url: 1000 + next_id,
+      timestamp: Math.floor (new Date ().getTime () / 1000)
+    });
+
+    res.end (JSON.stringify ({
+      original_url: req.params.link,
+      short_url: base_url + (1000 + next_id)
+    }));
+    db.close ();
+  });
+});
+
 app.get ('*', (req, res) => {
   res.writeHead (200, { 'Content-Type': 'text/html'});
+
   let buf = fs.readFileSync ('README.md');
+
   res.end ('<html><head><title>Neckers Url Shortener Microservice</title></head><body>'
           + marked (buf.toString ())
           + '</body></html>');
